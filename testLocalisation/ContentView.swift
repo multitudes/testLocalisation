99167209422fe9d21de69f71bf0d7fef1b29f429
laurent <wrmultitudes@gmail.com>
2020-10-09 12:13:50 +0200

    Initial Commit

---
+++ testLocalisation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testLocalisation
+//
+//  Created by Laurent B on 09/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testLocalisation/testLocalisationApp.swift
@@ -0,0 +1,17 @@
+//
+//  testLocalisationApp.swift
+//  testLocalisation
+//
+//  Created by Laurent B on 09/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct testLocalisationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
